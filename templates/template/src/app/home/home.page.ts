68c6f3694e24beec1b3f69d3745aeb57a2f61cd5
Well-199 <wellington.marunaka@gmail.com>
2022-03-14 07:20:42 -0300

    0.0.1

--- templates/template/src/app/home/home.page.ts
+++ templates/template/src/app/home/home.page.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
+  mensagem : string = "Ola Mundo"
+
   constructor() {}
+
+  mudarMensagemAoClicar(){
+    this.mensagem = 'Ola Brasil'
+
+    setTimeout(() => {
+      this.mensagem = 'Ola Rio de Janeiro'
+    }, 1000)
+  }
 
 }
