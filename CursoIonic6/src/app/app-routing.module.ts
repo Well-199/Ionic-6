fb002102090720653a53432a0246b76f8a364c44
Well-199 <wellington.marunaka@gmail.com>
2022-03-14 07:39:27 -0300

    first pages

--- CursoIonic6/src/app/app-routing.module.ts
+++ CursoIonic6/src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'button',
+    loadChildren: () => import('./pages/button/button.module').then( m => m.ButtonPageModule)
   }
 ];
 
